3e85260809566236506769a5bd757d5b36d5dfc9
arrozconhuevofrito <luisbryann080403@gmail.com>
2024-06-18 02:55:32 -0500

    Inicio del proyecto, server creado

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT ?? 1234;
+
+
+app.listen(PORT, () => console.log(`App escuchando en http://localhost:${PORT}`));
