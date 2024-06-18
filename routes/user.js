fd5f4d4a455ab1ae18f9fc61c45bc37f1df47ad3
Bryan-CO <luisbryann080403@gmail.com>
2024-06-18 14:53:19 -0500

    Se ruteó el user con el UserController

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,13 @@
 import { randomUUID } from 'node:crypto';
 import { Router } from 'express';
+import { UserController } from '../controllers/user.js';
 
 export const userRouter = Router();
 
-userRouter.get('/', (req, res) => res.send('HOLAAAAA'));
+userRouter.get('/', UserController.getAll);
+
+userRouter.get('/:id', UserController.getById);
+
+userRouter.post('/', UserController.create);
+
+userRouter.patch('/:id', UserController.update);
