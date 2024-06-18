8902c61228d2250aab32222dc5fbc9711b8f8f1f
Bryan-CO <luisbryann080403@gmail.com>
2024-06-18 16:53:36 -0500

    Se configuró el UserController con los metodos correspondientes (res, req)

--- controllers/user.js
+++ controllers/user.js
@@ -1,12 +1,26 @@
+import { UserModel } from "../models/user.js";
+import { validateUser } from "../schemas/user.js";
+
 export class UserController{
     static async getAll(req, res) {
-        res.send('Se listaron todos los usuarios uwu');
+        const users = await UserModel.getAll();
+        res.json(users);
     }
+
     static async getById(req, res) {
         res.send('Se listó un usuario por ID');
     }
+
     static async create(req, res) {
-        res.send('Se creó un nuevo alumno uwu');
+        const result = validateUser(req.body);
+
+        //console.log(JSON.parse(result)); Esto no, porque el JSON.parse sirve para convertir un JSON a un objeto
+
+        if (result.error) {
+            return res.status(400).json({ error: JSON.parse(result.error.message) })
+        }
+        const newUser = await UserModel.create({input: result.data});
+        res.status(201).json(newUser);
     }
     static async update(req, res) {
         res.send('Se actualizó un alumno uwu');
