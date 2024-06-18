3045a7721b1f739ff21874913eeb742a3b80c8d5
Bryan-CO <luisbryann080403@gmail.com>
2024-06-18 16:52:58 -0500

    Se configuró el UserModel con los metodos correspondientes y se añadió la conexion a mysql

--- models/user.js
+++ models/user.js
@@ -1,17 +1,49 @@
+import { randomUUID } from 'node:crypto';
+import mysql from 'mysql2/promise.js';
+
+const config = {
+    host: 'localhost',
+    user: 'root',
+    password: 'mysqladmin',
+    database: 'RED_SOCIAL'
+}
+
+const connection = await mysql.createConnection(config);
+
 export class UserModel {
-    static getAll(){
+    static async getAll(){
+        return {status: 200, message: "Se listaron los usuarios xd"};
+    }
+    static async getById(){
+
+    }
+    static async create({ input }){
+        const{
+            alias,
+            nombre,
+            apellido,
+            email,
+            contraseña,
+            rutaAvatar
+        } = input;
+
+        const result = await connection.query(
+            `INSERT INTO tb_usuario (Alias, Nombre, Apellido, Email, Contraseña, RutaAvatar) VALUES
+(?, ?, ?, ?, ?, ?);`,[alias, nombre, apellido, email, contraseña, rutaAvatar]
+        );
+
+        // const newUser = {
+        //     id: randomUUID(),
+        //     // alias: result.data.alias, // podemos hacerlo así, o...
+        //     ...input
+        // }
         
+        console.log(result);
     }
-    static getById(){
+    static async update(){
 
     }
-    static create(){
+    static async delete(){
 
-    }
-    static update(){
-
-    }
-    static delete(){
-        
     }
 }
