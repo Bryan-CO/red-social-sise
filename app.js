47396d265ce601af72aa5c10a79a902a72108a56
Bryan-CO <luisbryann080403@gmail.com>
2024-06-19 01:44:06 -0500

    Se creo metodo require y se usó el port del config.json

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ import express from 'express';
 import { userRouter } from './routes/user.js';
 import { status404 } from './middlewares/status404.js';
 import { corsMiddleware } from './middlewares/cors.js';
+import { createRequire } from 'node:module';
+const require = createRequire(import.meta.url);
+const config = require('./config.json');
 
 const app = express();
-const PORT = process.env.PORT ?? 1234;
+const PORT = config.development.server.port ?? 3000;
 
 app.use(express.json());
 app.use(corsMiddleware());
