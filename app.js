a239fee50f00a4c1968216d4651064d555ad9acf
sDorot <gdsebastian.24@gmail.com>
2024-06-23 14:59:28 -0500

    Se cambio la configuracion del puerto para usar .env

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ import express from 'express';
 import { userRouter } from './routes/user.js';
 import { status404 } from './middlewares/status404.js';
 import { corsMiddleware } from './middlewares/cors.js';
-import { createRequire } from 'node:module';
 import { publicacionRouter } from './routes/publication.js';
 import { errorHandler } from './middlewares/errorHandler.js';
+// import { createRequire } from 'node:module';
 
-const require = createRequire(import.meta.url);
-const config = require('./config.json');
+// const require = createRequire(import.meta.url);
+// const config = require('./config.json');
 
 const app = express();
-const PORT = config.development.server.port ?? 3000;
+const PORT = process.env.SERVER_PORT ?? 3000;
 
 app.use(express.json());
 app.use(corsMiddleware());
