755c41ab8e2aef3fb6f3f479900796fce70ced14
sDorot <gdsebastian.24@gmail.com>
2024-06-26 17:58:16 -0500

    Se creo AuthUtils para el uso de las dependencias

---
+++ src/utils/AuthUtil.js
@@ -0,0 +1,46 @@
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcryptjs'
+
+export class AuthUtil {
+
+    // GENERATE TOKEN WITH JWT
+    static async generateToken( payload ) {
+        return new Promise((resolve, reject) => {
+            jwt.sign(
+                { id: payload.id, username: payload.username, role: payload.role },
+                process.env.JWT_SECRET,
+                { expiresIn: '3d' },
+                (err, token) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(token);
+                    }
+                }
+            );  
+        });
+    }
+
+    // GENERATE PASSWORD WITH BCRYPT
+    static async hashPassword( password ) {
+        try {
+            const rounds = parseInt(process.env.BCRYPT_SALT);
+            const salt = await bcrypt.genSalt(rounds);
+            const hashedPass = await bcrypt.hash(password, salt);
+            return hashedPass;
+        } catch (err) { 
+            console.log(err);
+        }
+    }
+
+    // COMPARE PASSWORD 
+    static async comparePassword( password, hashedPassword ) {
+        try {
+            const validPassword = await bcrypt.compare(password, hashedPassword);
+            return validPassword;
+        } catch (err) {
+            console.log(err);
+        }
+    }  
+    
+}
